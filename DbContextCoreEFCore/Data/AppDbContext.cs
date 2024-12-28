67f2d19c8e96748f247aa43a7718c975497fde76
Tejashree Bhosale <122776618+Teja-212121@users.noreply.github.com>
2024-12-28 19:15:21 +0530

    Dbcontext Added

---
+++ DbContextCoreEFCore/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbContextCoreEFCore.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+    }
+}
